d22dd3b528fc726f262799cfb8f3653a043dfddd
Roberto267 <114701095+Roberto267@users.noreply.github.com>
2023-04-21 01:31:54 -0600

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,48 @@
+export const imgs = [
+	{
+		id: 1,
+		img: '/img/LMESSI.png',
+		alt: 'LM10',
+	},
+	{
+		id: 2,
+		img: '/img/CR7.png',
+		alt: 'CR7',
+	},
+	{
+		id: 3,
+		img: '/img/Courtois.png',
+		alt: 'courtois',
+	},
+	{
+		id: 4,
+		img: '/img/lukamodric.png',
+		alt: 'luka',
+	},
+	{
+		id: 5,
+		img: '/img/mba.png',
+		alt: 'mba',
+	},
+	{
+		id: 6,
+		img: '/img/halaadn.png',
+		alt: 'halaadn',
+	},
+	{
+		id: 7,
+		img: '/img/salah.png',
+		alt: 'salah',
+	},
+	{
+		id: 8,
+		img: '/img/Virgil.png',
+		alt: 'Virgil',
+	},
+	{
+		id: 9,
+		img: '/img/sergio.png',
+		alt: 'sergio',
+	},
+	{ id: 10, img: '/img/Neymar.png', alt: 'NEY' },
+];
